d921d782a2bec4a1700b7711938182e41e090e37
Kyle Flynn <kyle.flynn5798@yahoo.com>
2016-02-28 18:27:30 -0500

    Added "REAL_ROBOT" parameter (-r)

--- src/main.cpp
+++ src/main.cpp
@@ -59,38 +59,7 @@ struct ProgParams
 	bool Debug;
 	bool Process;
 	bool USB_Cam;
-};
-
-//Struct to hold information about targets found
-struct Target
-{
-	Rect HorizontalTarget;
-	Rect VerticalTarget;
-
-	double HorizontalAngle;
-	double VerticalAngle;
-	double Horizontal_W_H_Ratio;
-	double Horizontal_H_W_Ratio;
-	double Vertical_W_H_Ratio;
-	double Vertical_H_W_Ratio;
-
-	Point HorizontalCenter;
-	Point VerticalCenter;
-
-	bool HorizGoal;
-	bool VertGoal;
-	bool HotGoal;
-	bool matchStart;
-	bool validFrame;
-
-	//camera bool
-	bool cameraConnected;
-
-	int targetLeftOrRight;
-	int lastTargerLorR;
-	int hotLeftOrRight;
-	double targetDistance;
-
+	bool Real_Robot;
 };
 
 //function declarations
@@ -100,9 +69,6 @@ void printCommandLineUsage();
 void initializeParams(ProgParams& params);
 double diffClock(timespec start, timespec end);
 Mat ThresholdImage(Mat img);
-void findTarget(Mat original, Mat thresholded, Target& targets, const ProgParams& params);
-void NullTargets(Target& target);
-void CalculateDist(Target& targets);
 void error(const char *msg);
 
 //Threaded Video Capture Function
@@ -110,24 +76,6 @@ void *VideoCap(void *args);
 
 //GLOBAL CONSTANTS
 const double PI = 3.141592653589793;
-
-//Thresholding parameters
-int minR = 0;
-int maxR = 30;
-int minG = 80; //160 for ip cam, 80 to support MS webcam
-int maxG = 255;
-int minB = 0;
-int maxB = 30;
-
-//Target Ratio Ranges
-double MinHRatio = 1.5;
-double MaxHRatio = 6.6;
-
-double MinVRatio = 1.5;
-double MaxVRatio = 8.5;
-
-int MAX_SIZE = 255;
-
 //Some common colors to draw with
 const Scalar RED = Scalar(0, 0, 255),
 			BLUE = Scalar(255, 0, 0),
@@ -149,7 +97,6 @@ pthread_t AutoCounter;
 
 
 //Store targets in global variable
-Target targets;
 Mat frame;
 
 //Global Timestamps for auto
@@ -166,14 +113,21 @@ int main(int argc, const char* argv[])
 	ProgParams params;
 	parseCommandInputs(argc, argv, params);
 
-
 	// setup Network tables for this client to talk to the java code (same place this code is running!)
 	NetworkTable::SetClientMode();
-	NetworkTable::SetIPAddress("10.36.18.22"); // where is the robot?
+
+	string networktables_ip;
+
+	if (params.Real_Robot) {
+		string networktables_ip = "10.36.18.79";
+	} else {
+		string networktables_ip = "10.36.18.22";
+	}
+
+	NetworkTable::SetIPAddress(networktables_ip); // where is the robot?
 	std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("SmartDashboard"); // what table will we interface with?
 
 	cout << "Got through the network tables\n";
-
 
 	//start mjpeg stream thread
 	pthread_create(&MJPEG, NULL, VideoCap, &params);
@@ -181,11 +135,7 @@ int main(int argc, const char* argv[])
 	//Create Local Processing Image Variables
 	Mat img, thresholded, output;
 
-
 	//initialize variables so processing loop is false;
-	targets.matchStart = false;
-	targets.validFrame = false;
-	targets.hotLeftOrRight = 0;
 	progRun = false;
 
 	struct timespec start, end;
@@ -230,14 +180,8 @@ int main(int argc, const char* argv[])
 						}
 					}
 
-//					cout << "set targetContour" << endl;
-
 					Mat drawing = Mat::zeros( thresholded.size(), CV_8UC3 );
 					Scalar color = Scalar( 255, 255, 255);
-
-//					cout << "drew targetContour" << endl;
-
-					// find the extents of each contour
 
 					vector <Point> hull;
 
@@ -263,9 +207,6 @@ int main(int argc, const char* argv[])
 						else
 						  cornerSq[i] = (img.cols - hull[0].x) * hull[0].y;
 					}
-
-//					cout << "hull " << "0" << ": (" << hull[0].x << ", " << hull[0].y << ") ";
-
 
 					for (unsigned int i = 1; i < hull.size(); i++) {
 
@@ -294,10 +235,6 @@ int main(int argc, const char* argv[])
 					}
 
 					cout << endl;
-
-//					for (unsigned int i = 0; i < corners.size(); i++) {
-//						cout << "corner " << i << ": (" << corners[i].x << ", " << corners[i].y << ")" << endl;
-//					}
 
 
 					for (unsigned int i = 0; i < corners.size(); i++) {
@@ -367,18 +304,7 @@ int main(int argc, const char* argv[])
 				imwrite("/var/local/natinst/www/capture-test.png", img);
 
 
-				//Lock Targets and determine goals
 				pthread_mutex_lock(&targetMutex);
-//				findTarget(img, thresholded, targets, params);
-//				CalculateDist(targets);
-
-//				if(params.Debug)
-//				{
-//					cout<<"Vert: "<<targets.VertGoal<<endl;
-//					cout<<"Horiz: "<<targets.HorizGoal<<endl;
-//					cout<<"Hot Goal: "<<targets.HotGoal<<endl;
-//					cout<<"Dist:" <<targets.targetDistance<<endl<<endl;
-//				}
 				pthread_mutex_unlock(&targetMutex);
 
 				clock_gettime(CLOCK_REALTIME, &end);
@@ -407,244 +333,7 @@ int main(int argc, const char* argv[])
 
 }
 
-///////////////////FUNCTIONS/////////////////////
 
-//int main(){
-//   VideoCapture cap(0); //capture the video from webcam
-//
-//   if (!cap.isOpened()) {
-//        cout << "Cannot open the web cam" << endl;
-//        return -1;
-//   }
-//
-//   int iLowH = 0;
-//   int iHighH = 179;
-//
-//   int iLowS = 0;
-//   int iHighS = 255;
-//
-//   int iLowV = 250;
-//   int iHighV = 255;
-//
-//   while (true) {
-//       Mat imgOriginal;
-//
-//       bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-//
-//       if (!bSuccess) {
-//            cout << "Cannot read a frame from video stream" << endl;
-//            break;
-//       }
-//
-//       Mat imgHSV;
-//
-//       cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to B&W
-//
-//       Mat imgThresholded;
-//
-//       inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-//
-//       //morphological opening (remove small objects from the foreground)
-//       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-//       dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-//
-//       //morphological closing (fill small holes in the foreground)
-//       dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-//       erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-//
-////       vector <vector<Point> > contours;
-//
-////       findContours(imgThresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-//
-////       imwrite("/var/local/natinst/www/capture.jpg", imgThresholded);
-//
-////       if (waitKey(30) == 27) { //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-////           cout << "esc key is pressed by user" << endl;
-////
-////           break;
-////       }
-//   }
-//
-//   return 0;
-//
-//}
-
-/**
- * This function uses the law of lense projection to
- * estimate the distance to an object of known height only
- * using a single camera.
- *
- * This function uses only the vertical target height, the
- * pixel height of the image, and the view angle of the
- * camera lense.
- */
-void CalculateDist(Target& targets)
-{
-	//vertical target is 32 inches fixed
-	double targetHeight = 32.0;
-
-	//get vertical pixels from targets
-	int height = targets.VerticalTarget.height;
-
-	//d = Tft*FOVpixel/(2*Tpixel*tanΘ)
-	targets.targetDistance = Y_IMAGE_RES * targetHeight
-			/ (height * 12 * 2 * tan(VIEW_ANGLE * PI / (180 * 2)));
-}
-
-/**
- * This function scans through an image and determins
- * if rectangles exist which match the profile of a
- * "Hot Goal".
- *
- * The "Hot Goal" is specific to the 2014 FRC game
- * and is identified as a horizontal and vertical target
- * in the same frame, with known width and height.
- */
-void findTarget(Mat original, Mat thresholded, Target& targets, const ProgParams& params)
-{
-	vector<Vec4i> hierarchy;
-	vector<vector<Point> > contours;
-
-	//Find rectangles
-	findContours(thresholded, contours, hierarchy, RETR_EXTERNAL,
-			CHAIN_APPROX_SIMPLE);
-
-	if(params.Debug)
-	{
-	cout << "Contours: " << contours.size() << endl;
-	cout << "Hierarchy: " << hierarchy.size() << endl;
-	}
-
-	//run through all contours and remove small contours
-	unsigned int contourMin = 6;
-	for (vector<vector<Point> >::iterator it = contours.begin();
-			it != contours.end();)
-	{
-		//cout<<"Contour Size: "<<it->size()<<endl;
-		if (it->size() < contourMin)
-			it = contours.erase(it);
-
-		else
-			++it;
-
-	}
-
-	//Vector for Min Area Boxes
-	vector<RotatedRect> minRect(contours.size());
-
-	/// Draw contours
-
-	NullTargets(targets);
-
-	//run through large contours to see if they are our targerts
-	if (!contours.empty() && !hierarchy.empty())
-	{
-
-		for (unsigned int i = 0; i < contours.size(); i++)
-		{
-			//capture corners of contour
-			minRect[i] = minAreaRect(Mat(contours[i]));
-
-			if(params.Visualize)
-			{
-
-				//if(hierarchy[i][100] != -1)
-				//drawContours(original, contours, i, RED, 2, 8, hierarchy, 0,Point());
-
-				//draw a minimum box around the target in green
-				Point2f rect_points[4];
-				minRect[i].points(rect_points);
-				for (int j = 0; j < 4; j++)
-					line(original, rect_points[j], rect_points[(j + 1) % 4], BLUE, 1, 8);
-			}
-			//define minAreaBox
-			Rect box = minRect[i].boundingRect();
-
-			double WHRatio = box.width / ((double) box.height);
-			double HWRatio = ((double) box.height) / box.width;
-
-			//check if contour is vert, we use HWRatio because it is greater that 0 for vert target
-			if ((HWRatio > MinVRatio) && (HWRatio < MaxVRatio))
-			{
-				targets.VertGoal = true;
-				targets.VerticalTarget = box;
-				targets.VerticalAngle = minRect[i].angle;
-				targets.VerticalCenter = Point(box.x + box.width / 2,
-						box.y + box.height / 2);
-				targets.Vertical_H_W_Ratio = HWRatio;
-				targets.Vertical_W_H_Ratio = WHRatio;
-
-			}
-			//check if contour is horiz, we use WHRatio because it is greater that 0 for vert target
-			else if ((WHRatio > MinHRatio) && (WHRatio < MaxHRatio))
-			{
-				targets.HorizGoal = true;
-				targets.HorizontalTarget = box;
-				targets.HorizontalAngle = minRect[i].angle;
-				targets.HorizontalCenter = Point(box.x + box.width / 2,
-						box.y + box.height / 2);
-				targets.Horizontal_H_W_Ratio = HWRatio;
-				targets.Horizontal_W_H_Ratio = WHRatio;
-			}
-
-			if (targets.HorizGoal && targets.VertGoal)
-			{
-				targets.HotGoal = true;
-
-				//determine left or right
-				if (targets.VerticalCenter.x < targets.HorizontalCenter.x) //target is right
-					targets.targetLeftOrRight = 1;
-				else if (targets.VerticalCenter.x > targets.HorizontalCenter.x) //target is left
-					targets.targetLeftOrRight = -1;
-
-				targets.lastTargerLorR = targets.targetLeftOrRight;
-
-			}
-
-			if(params.Debug)
-			{
-				cout<<"Contour: "<<i<<endl;
-				cout<<"\tX: "<<box.x<<endl;
-				cout<<"\tY: "<<box.y<<endl;
-				cout<<"\tHeight: "<<box.height<<endl;
-				cout<<"\tWidth: "<<box.width<<endl;
-				cout<<"\tangle: "<<minRect[i].angle<<endl;
-				cout<<"\tRatio (W/H): "<<WHRatio<<endl;
-				cout<<"\tRatio (H/W): "<<HWRatio<<endl;
-				cout<<"\tArea: "<<box.height*box.width<<endl;
-			}
-
-			//ID the center in yellow
-			Point center(box.x + box.width / 2, box.y + box.height / 2);
-			line(original, center, center, YELLOW, 3);
-			line(original, Point(320/2, 240/2), Point(320/2, 240/2), YELLOW, 3);
-
-		}
-		//if(params.Visualize)
-			//imshow("Contours", original); //Make a rectangle that encompasses the target
-	}
-	else
-	{
-		cout << "No Contours" << endl;
-		targets.targetLeftOrRight = 0;
-	}
-
-	if(params.Visualize)
-				imshow("Contours", original); //Make a rectangle that encompasses the target
-
-	pthread_mutex_lock(&matchStartMutex);
-	if (!targets.matchStart)
-		targets.hotLeftOrRight = targets.targetLeftOrRight;
-	pthread_mutex_unlock(&matchStartMutex);
-
-}
-
-/**
- * This function performs numerous filtering on
- * a color image in order to only return
- * areas of interest based on their color
- *
- */
 Mat ThresholdImage(Mat original)
 {
 
@@ -675,30 +364,6 @@ Mat ThresholdImage(Mat original)
 	return imgThresholded;
 
 }
-
-/**
- * This functions "zeros", the targets identified
- * so that a clean slate can be used to determine
- *] if the next image contains targets as well.
- */
-void NullTargets(Target& target)
-{
-
-	target.HorizontalAngle = 0.0;
-	target.VerticalAngle = 0.0;
-	target.Horizontal_W_H_Ratio = 0.0;
-	target.Horizontal_H_W_Ratio = 0.0;
-	target.Vertical_W_H_Ratio = 0.0;
-	target.Vertical_H_W_Ratio = 0.0;
-	target.targetDistance = 0.0;
-	target.targetLeftOrRight = 0;
-	target.lastTargerLorR = 0;
-
-	target.HorizGoal = false;
-	target.VertGoal = false;
-	target.HotGoal = false;
-
-}
 void initializeParams(ProgParams& params)
 {
 	params.Debug = false;
@@ -708,7 +373,7 @@ void initializeParams(ProgParams& params)
 	params.Visualize = false;
 	params.Process = true;
 	params.USB_Cam = false;
-
+	params.Real_Robot = false;
 }
 
 /**
@@ -774,6 +439,10 @@ void parseCommandInputs(int argc, const char* argv[], ProgParams& params)
 			else if (string(argv[i]) == "-np") //no processing
 			{
 				params.Process = false;
+			}
+			else if (string(argv[i]) == "-r") // Using competition robot
+			{
+				params.Real_Robot = true;
 			}
 			else if (string(argv[i]) == "-v") //Enable Visual output
 			{
@@ -930,7 +599,6 @@ void *VideoCap(void *args)
 
 		//set true boolean
 		pthread_mutex_lock(&targetMutex);
-		targets.cameraConnected = true;
 		pthread_mutex_unlock(&targetMutex);
 
 		//end clock to determine time to setup stream
